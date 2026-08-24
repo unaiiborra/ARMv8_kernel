ab6c50b9a98d0147ce6fa32de4be783a806b2889
Unai Iborra Albeniz <iborra.unai@gmail.com>
2026-08-24 15:57:35 +0200

    Fix default value of EMBEDDED_BINARIES_PATH

--- make/Folders.mk
+++ make/Folders.mk
@@ -1,8 +1,8 @@
-BIN_DIR    					:= bin
+BIN_DIR    				:= bin
 SRC_DIR     				:= src
 INCLUDE_DIR 				:= include
 MAP_DIR						:= $(BIN_DIR)
 OBJ_DIR						:= $(BIN_DIR)/obj
 RS_LIB_DIR  				:= target/$(RS_TARGET)/release/libimx8mp_kernel.a
 CROSS_COMPILE_PATH			?= /usr/lib/gcc/aarch64-none-elf/15.2.1
-EMBEDDED_USR_BINARIES_PATH  ?= user_examples
+EMBEDDED_BINARIES_PATH	 		?= embedded
